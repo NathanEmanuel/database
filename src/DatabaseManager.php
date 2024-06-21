b8a1214633f6add79571b721bbfd68eb7a4cd2f6
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-06-21 13:32:56 +0200

    add abstract base class DatabaseManager

---
+++ src/DatabaseManager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Compucie\DatabaseManagers;
+
+use Exception;
+use mysqli;
+
+abstract class DatabaseManager
+{
+    private mysqli $client;
+
+    public function __construct(string $configpath)
+    {
+
+        if (!file_exists($configpath)) {
+            throw new FileNotFoundException();
+        }
+        $config = parse_ini_file($configpath);
+
+        $this->client = new mysqli(...$config);
+    }
+}
+
+class FileNotFoundException extends Exception
+{
+}
+
+class CouldNotInsertException extends Exception
+{
+}
