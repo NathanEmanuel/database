52a2d28893f5730a94a305880d9b5bc16488c704
Djojomoenawie, N.E. (Nathan, Student M-CS) <n.e.djojomoenawie@student.utwente.nl>
2024-08-12 10:53:26 +0200

    implement getMemberIdsWithBirthdayToday

--- src/Member/BirthdaysTableManager.php
+++ src/Member/BirthdaysTableManager.php
@@ -21,4 +21,28 @@ trait BirthdaysTableManager
         $statement->execute();
         $statement->close();
     }
+
+    /**
+     * Return the member IDs of members whose birthday is today.
+     * @return  int[]   The member IDs
+     * @throws  mysqli_sql_exception
+     */
+    public function getMemberIdsWithBirthdayToday(): array
+    {
+        $statement = $this->getClient()->prepare(
+            "SELECT `member_id` FROM `screen_birthdays`
+            WHERE DAY(date_of_birth) = DAY(CURRENT_DATE())
+		    AND MONTH(date_of_birth) = MONTH(CURRENT_DATE());"
+        );
+        $statement->bind_result($memberId);
+        $statement->execute();
+
+        $memberIds = array();
+        while ($statement->fetch()) {
+            $memberIds[] = $memberId;
+        }
+
+        $statement->close();
+        return $memberIds;
+    }
 }
